484c0fedb2ba4bb5943b8c0e890a8c83eacf0744
M57 <hiari45@gmail.com>
2024-03-04 14:06:48 +0300

    first commit

---
+++ clock.js
@@ -0,0 +1,27 @@
+function updateClock() {
+let currentDate = new Date();
+
+let hours = currentDate.getHours();
+let minutes = currentDate.getMinutes();
+let seconds = currentDate.getSeconds();
+
+if (hours > 12) {
+    hours -= 12;
+    meridiem = "PM";
+} else if (hours === 0) {
+    hours = 12;
+}
+if (hours < 10) {
+    hours = "0" + hours;
+}
+if (minutes < 10) {
+    minutes = "0" + minutes;
+}
+if (seconds < 10) {
+    seconds = "0" + seconds;
+}
+
+document.getElementById("clock").textContent = hours + ":" +minutes ;
+}
+updateClock();
+setInterval(updateClock, 15000);
